2ffbe023dfecf7f0cc43b028733685e6f355cc3f
Cameron Hudson <camnn3@yahoo.com>
2020-06-17 14:53:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(number)
   elsif number.length == 2
     return number.join(" and ")
   elsif number.length > 2
-    return number[0...-1].join(", ") << ", and #{number[-1]}"
+    return number[0...-1].join(", ") << "and #{number[-1]}"
   end
 end
 
