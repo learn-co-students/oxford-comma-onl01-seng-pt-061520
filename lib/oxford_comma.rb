1ef3322640557b784567db335735ef2aa3577bc3
IgoVeyner <igorveyner95@gmail.com>
2020-06-17 15:46:24 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+    length = array.length()
+    if length == 1
+        result = array.join
+        return result
+    end
 
+    last = array.last
+    array.pop()
+
+    if length == 2
+        result = array.join()
+        return "#{result} and #{last}"
+    else 
+        result = array.join(", ")
+        return "#{result}, and #{last}"
+    end
 end
