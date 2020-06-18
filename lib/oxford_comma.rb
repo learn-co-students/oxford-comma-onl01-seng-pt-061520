0837fa9d5004210fdc480bee430c417aa084cd13
ajkelso <ajkelso@gmail.com>
2020-06-18 14:20:18 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length > 2
+  ending = array.pop
+  ending = ", and #{ending}"
+  beginning = array.join(", ")
+  beginning + ending
+  else
+    array.join(" and ")
+end
 end
