e6c31c377d04608a3d1f3b139e0f605645b71a33
Teodoro Garcia <teodorogarcia@teodoros-air.lan>
2020-06-23 19:47:58 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+  array = ["fiddleheads","okra", "kohlrabi"]
+  
+  def oxford_comma(array)
+    #array.join ", " 
+    #array.join " and "
+   if array.length == 1
+    return array.join " and "
+   end
 
+   if array.length == 2
+    return array.join " and " 
+   end 
+
+   if array.length > 2
+    temp = array.pop 
+    return "#{array.join(", ")}" + ", and #{temp}"
+   end
 end
