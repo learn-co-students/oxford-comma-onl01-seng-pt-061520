42e136bf2511dbe67eb46d05b383741b4c8514bd
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-29 14:49:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
-end
+
+def oxford_comma(array)
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+     array.last.insert(0, "and ")
+      return array.join(", ")
+   end
+ end
+
+  
